287abeec6fabb3578976d4997e1a689296c6488a
Alexis Archambault <alexis2731link@gmail.com>
2020-04-26 17:22:42 -0400

    Fin Tp5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'Inter-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Interventions';
+  faBell = faBell;
 }
